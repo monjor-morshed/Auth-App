f330f48a5592b416d712a428a0ceef1ad965decb
monjor-morshed <2020-1-60-227@std.ewubd.edu>
2024-05-06 22:04:02 +0600

    signup api done

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,36 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
+import userRoutes from "./routes/userRoute.js";
+import authRoutes from "./routes/authRoute.js";
+import { stat } from "fs";
+
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
     console.log("Connected to MongoDB");
+    app.listen(5000, () => {
+      console.log("Server is running on port 5000");
+    });
   })
   .catch((error) => {
     console.log(error);
   });
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    error: message,
+    statusCode,
+  });
 });
