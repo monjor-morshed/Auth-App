85584f78d58313c1a9580c2122950039caa2ee63
monjor-morshed <2020-1-60-227@std.ewubd.edu>
2024-05-06 17:36:21 +0600

    pages created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
-// import React from 'react'
-
-import Header from "./components/Header"
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
-    <div>
-      <Header />
-    </div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+    </Routes>
+  );
+};
 
-export default App
+export default App;
